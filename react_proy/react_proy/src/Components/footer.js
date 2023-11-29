dfbc13cc6ca4ff8d23107bf969a677fd804dae04
David Esteban Echeverri <121136952+DAVID25051991@users.noreply.github.com>
2023-11-29 10:16:37 -0500

    deec

---
+++ react_proy/react_proy/src/Components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/boton.css'
+
+const footer = () => {
+  return (
+    <footer>
+        <p className='footer'>realizado por: David Esteban Echeverri.</p>
+    </footer>
+  )
+}
+
+export default footer
